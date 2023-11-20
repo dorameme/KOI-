a43eaa50bc04058fa3749fc8ad45b4a4caa3da0e
dorameme <doradorav@naver.com>
2023-11-20 14:03:40 +0900

    stack file

--- 스택/KOI17608막대기bronze2.cpp
+++ 스택/KOI17608막대기bronze2.cpp
@@ -1,3 +1,11 @@
+/*
+start:13:50
+end:13:55
+시간복잡도:O(N)
+공간복잡도:O(N)
+풀이:스택으로 풀수도 있지만 배열에 넣어서 풀어도 된다.
+간단하게 맨끝부터 봐서 나를 포함한 답 1 에서 나보다 큰애가 나올떄만 +1 씩해주면 된다.
+*/
 #include <iostream>
 #include <stack>
 using namespace std;
