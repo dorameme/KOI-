b3c491c60b6e48c092edfff78eaa54a4b78d734c
dorameme <doradorav@naver.com>
2023-11-21 13:51:40 +0900

    stack file

--- 스택/KOI9935문자열폭발gold4.cpp
+++ 스택/KOI9935문자열폭발gold4.cpp
@@ -1,4 +1,12 @@
-#include <iostream>
+/*
+start:13:00
+end:13:47
+시간복잡도:O(NM)(s사이즈,bomb사이즈)
+공간복잡도:O(N)
+풀이:여러풀이가 있다..근데 이게 제일쉬운 풀이인것같다. ㅠㅠ
+난이도에 비해 풀기 쉬운문제는 아니고 아이디어가 중요하다 나는 맨뒤에서 길이가 bombsize 이상일때 bomb이랑 맨끝이 같다면 계속 제거해주는 방식으로 풀었다.
+*/ 
+#include<iostream>
 #include <string>
 #include <stack>
 using namespace std;
@@ -11,19 +19,28 @@ int main()
     for (int i = 0; i < s.size(); i++)
     {
         answer += s[i];
-        if(s[i]==bomb.back() && answer.size()>=bomb.size()){
-            cout<<answer<<"\n";
-            bool flag=true;
-            for(int j=0;j<bomb.size();j++){
-               if(answer[answer.size()-1-j]!=bomb[bomb.size()-1-j]){flag=false;break;}
+        if (s[i] == bomb.back() && answer.size() >= bomb.size())
+        {
+            bool flag = true;
+            for (int j = 0; j < bomb.size(); j++)
+            {
+                if (answer[answer.size() - 1 - j] != bomb[bomb.size() - 1 - j])
+                {
+                    flag = false;
+                    break;
+                }
             }
-            if(flag){
-                for(int j=0;j<bomb.size();j++){
-                answer.pop_back();
+            if (flag)
+            {
+                for (int j = 0; j < bomb.size(); j++)
+                {
+                    answer.pop_back();
                 }
             }
         }
     }
-    if(answer.empty())cout<<"FRULA";
-    else cout<<answer;
+    if (answer.empty())
+        cout << "FRULA";
+    else
+        cout << answer;
 }
